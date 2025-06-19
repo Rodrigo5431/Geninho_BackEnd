5cc7094e212c8f543e82302357c8952893588bee
Rodrigo5431 <rodrigokarvalho12890@gmail.com>
2025-06-18 23:08:19 -0300

    product sercice

--- Geninho/src/main/java/br/com/geninho/service/ProductService.java
+++ Geninho/src/main/java/br/com/geninho/service/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.geninho.dto.ProductDTO;
+import br.com.geninho.dto.ProductInsertDTO;
 import br.com.geninho.entities.Product;
 import br.com.geninho.exception.NotFoundException;
 import br.com.geninho.repository.ProductRepository;
@@ -31,4 +32,43 @@ public class ProductService {
 		return new ProductDTO(productOpt.get());
 	}
 	
+	public ProductDTO createProduct(ProductInsertDTO productInsert) {
+		
+		Product product = new Product();
+		product.setName(productInsert.getName());
+		product.setPrice(productInsert.getPrice());
+		product.setDescription(productInsert.getDescription());
+		product.setStock(productInsert.getStock());
+		productRepository.save(product);
+		
+		return new ProductDTO(product);
+	}
+	
+	public ProductDTO updateProduct (Long id, ProductInsertDTO productInsert) {
+		
+		Optional<Product>productOpt = productRepository.findById(id);
+		if (productOpt.isEmpty()) {
+			throw new NotFoundException("Produto não encontrado");
+		}
+		
+		Product product = new Product();
+		product.setId(id);
+		product.setName(productInsert.getName() != null? productInsert.getName() : productOpt.get().getName());
+		product.setPrice(productInsert.getPrice() != null? productInsert.getPrice() : productOpt.get().getPrice());
+		product.setDescription(productInsert.getDescription() != null? productInsert.getDescription() : productOpt.get().getDescription());
+		product.setStock(productInsert.getStock() != null? productInsert.getStock() : productOpt.get().getStock());
+		productRepository.save(product);
+		
+		return new ProductDTO(product);
+	}
+	
+	public String deleteProduct(Long id) {
+		
+		Optional<Product>productOpt = productRepository.findById(id);
+		if (productOpt.isEmpty()) {
+			throw new NotFoundException("Produto não encontrado");
+		}
+		productRepository.deleteById(id);
+		return "Produto deletado com sucesso";
+	}
 }
